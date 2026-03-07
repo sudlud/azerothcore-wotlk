99b2686300bd6a8ca2a27fb3c550bfae1ea2259d
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-03-07 11:44:14 +0100

    fix(DB/Ulduar): Destroyed Vehicles are now present in 25M. (#25017)

---
+++ data/sql/updates/pending_db_world/rev_1772826955.sql
@@ -0,0 +1,4 @@
+
+-- Set Spawn Masks.
+UPDATE `creature` SET `spawnMask` = `spawnMask` |2 WHERE (`id1` = 33059) AND (`guid` IN (136073, 136089, 136090));
+UPDATE `creature` SET `spawnMask` = `spawnMask` |2 WHERE (`id1` = 33063) AND (`guid` IN (136246, 136249));
