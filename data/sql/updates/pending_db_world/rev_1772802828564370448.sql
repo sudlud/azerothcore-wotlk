112a809888b6269a92a0e798e6d81ac9b5be777c
blinkysc <37940565+blinkysc@users.noreply.github.com>
2026-03-07 12:08:30 -0600

    fix(DB/Spell): Arcane Blast and Missile Barrage spell_proc corrections (#25015)
    Co-authored-by: blinkysc <blinkysc@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1772802828564370448.sql
@@ -0,0 +1,2 @@
+UPDATE `spell_proc` SET `SpellFamilyMask0` = 4096, `ProcFlags` = 0, `Charges` = 0 WHERE `SpellId` = 36032;
+UPDATE `spell_proc` SET `SpellFamilyMask0` = 0, `AttributesMask` = 8 WHERE `SpellId` = 44401;
