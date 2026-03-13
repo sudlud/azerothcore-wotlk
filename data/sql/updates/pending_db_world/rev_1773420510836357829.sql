0550b7778b4e901d64f600e7dd3f4c4381ced125
blinkysc <37940565+blinkysc@users.noreply.github.com>
2026-03-13 13:55:39 -0500

    fix(Core/Spells): Register missing Ice Barrier AuraScript for Incanter's Absorption (#25079)
    Co-authored-by: blinkysc <blinkysc@users.noreply.github.com>
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1773420510836357829.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = -11426 AND `ScriptName` = 'spell_mage_ice_barrier_aura';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(-11426, 'spell_mage_ice_barrier_aura');
